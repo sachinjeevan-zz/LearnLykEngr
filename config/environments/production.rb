ee63078e0850ca67c2b210f3219cf5ac5cf8d8e0
sachin nandha sabarish <sachinjeevan2506@gmail.com>
2018-11-28 17:45:46 +0530

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,5 +79,14 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = {host: 'https://edulearnlykengineer.herokuapp.com/'}
 
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name:      'sachinjeevan2506@gmail.com',
+    password:       'sachin2903',
+    domain:         'edulearnlykengineer.herokuapp.com',
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+}
 end
